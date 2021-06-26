36377a2f5fc49c6525d325787fba17c4cfd03b45
Сашка724ая <github@sashok724.net>
2021-06-26 23:57:08 +0300

    Update windows.sh

--- windows.sh
+++ windows.sh
@@ -1,26 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 # sashok724 was here
 set -euo pipefail
 
+if [ $# -lt 2 ]; then
+    echo 'Usage: <cpucores> <ramsize>'
+    exit 1
+fi
+
+readonly cpucores="$1"
+readonly ramsize="$2"
+
 # Too long to write options
-cpucores=8
-ramsize=20G
+readonly cpufeatures=+topoext,+invtsc,host-cache-info=on,l3-cache=on
+readonly hvflags=hv-passthrough=on,hv-spinlocks=0x1fff,hv-vendor-id=oknvidia
+readonly audioflags=in.frequency=48000,out.frequency=48000
+readonly driveflags=file.aio=threads,discard=unmap,cache.direct=on
 
-cpufeatures=+topoext,+invtsc,host-cache-info=on,l3-cache=on
-hvflags=hv-passthrough=on,hv-spinlocks=0x1fff,hv-vendor-id=oknvidia
+# Currently unneeded: (kvmflags are taken from kvm_default_props (qemu sources))
+#readonly kvmflags=kvm=off,+kvmclock,+kvm-nopiodelay,+kvm-asyncpf,+kvm-steal-time,+kvm-pv-eoi,+kvmclock-stable-bit,+x2apic,-acpi,-monitor
+#readonly hvflags=$hvflags,hv-vendor-id=fucknvidia
 
-audioflags=in.frequency=48000,out.frequency=48000
-driveflags=file.aio=threads,discard=unmap,cache.direct=on
-
-# Currently unneeded:
-#-drive file=/usr/share/virtio/virtio-win.iso,media=cdrom \
-#-blockdev driver=raw,file.driver=file,file.filename=".iso",$driveflags,read-only=on,node-name=cd-arch
-#-device scsi-cd,drive=cd-arch,bus=scsi.0,id=scsi-cd-arch
-
+#-device scsi-cd,drive=cd-arch,bus=scsi.0,id=scsi-cd-arch \
 #-device usb-audio,bus=xhci.0,port=1,audiodev=audiodev,id=usbaudio \
+#-device usb-host,bus=xhci.0,port=10,vendorid=0x0765,productid=0x5020,id=calibrator \
 #-device e1000,bus=pcie.0,mac=10:56:f2:d7:6f:9b,netdev=netdev,id=net \
 
-lgpath=/dev/shm/looking-glass-win
+readonly lgpath=/dev/shm/looking-glass-win
 touch "$lgpath"
 chmod 640 "$lgpath"
 chown sashok724:sashok724 "$lgpath"
@@ -30,19 +35,24 @@ jemalloc.sh qemu-system-x86_64 -no-user-config -nodefaults \
     -accel kvm,kernel-irqchip=on -no-hpet \
     -cpu host,check,enforce,migratable=no,kvm=on,$cpufeatures,$hvflags \
     -smp $(($cpucores*2)),sockets=1,cores=$cpucores,threads=2 -mem-prealloc -m ${ramsize} \
-    -global kvm-pit.lost_tick_policy=delay -global ICH9-LPC.disable_s3=1 -global ICH9-LPC.disable_s4=1 \
+    -global kvm-pit.lost_tick_policy=delay -global ICH9-LPC.disable_s3=1 \
     -overcommit cpu-pm=on,mem-lock=on \
     \
     -machine q35,dump-guest-core=off,mem-merge=off,vmport=off,pflash0=ovmf-code,pflash1=ovmf-vars \
-    -boot menu=on,strict=on,order=dc,splash-time=5000 -rtc base=utc,clock=host,driftfix=slew \
-    -monitor unix:"/run/windows/monitor.sock",server,nowait -vga none -display none -serial none -parallel none \
+    -boot menu=on,strict=on,order=dc,splash="/usr/local/lib/windows/splash.bmp",splash-time=5000 -rtc base=utc,clock=host,driftfix=slew \
+    -monitor unix:"/run/windows/monitor.sock",server,nowait -qmp unix:"/run/windows/qmp.sock",server,nowait \
     -spice addr=127.0.0.1,port=5900,disable-ticketing=on,seamless-migration=off \
+    -vga none -display none -serial none -parallel none \
     \
-    -chardev spicevmc,name=vdagent,id=vdagent \
     -audiodev pa,server="/run/user/1000/pulse/native",$audioflags,id=audiodev \
+    -chardev spicevmc,name=vdagent,id=vdagent \
+    -chardev socket,path="/run/windows/qga.sock",server=on,wait=off,name=qga,id=qga \
     -blockdev driver=raw,file.driver=file,file.filename="/usr/share/ovmf/x64/OVMF_CODE.fd",$driveflags,read-only=on,node-name=ovmf-code \
     -blockdev driver=raw,file.driver=file,file.filename="/usr/local/etc/windows/ovmf_vars.fd",$driveflags,read-only=off,node-name=ovmf-vars \
+    -blockdev driver=raw,file.driver=file,file.filename="/mnt/storage/Archive/Software/ISO Images/Windows11pre.iso",$driveflags,read-only=on,node-name=cd-win-install \
+    -blockdev driver=raw,file.driver=file,file.filename="/usr/share/virtio/virtio-win.iso",$driveflags,read-only=on,node-name=cd-win-virtio \
     -blockdev driver=raw,file.driver=host_device,file.filename="/dev/mapper/windows",$driveflags,read-only=off,node-name=drive-system \
+    -blockdev driver=raw,file.driver=file,file.filename="/mnt/storage/Images/QEMU/Windows/windowstest.img",$driveflags,read-only=off,node-name=drive-test \
     -netdev tap,ifname=tapwin,script=no,downscript=no,vhost=on,id=netdev \
     -object memory-backend-file,mem-path="$lgpath",size=256M,share=on,id=ivshmem-mem \
     -object input-linux,evdev="/dev/input/by-id/usb-Logitech_USB_Receiver-if02-event-mouse",id=imouse \
@@ -66,6 +76,10 @@ jemalloc.sh qemu-system-x86_64 -no-user-config -nodefaults \
     -device virtio-scsi-pci,bus=pcie.0,num_queues=8,iothread=thread-scsi,ioeventfd=on,id=scsi \
     -device virtio-net-pci,bus=pcie.0,netdev=netdev,mac=10:56:f2:d7:6f:9b,mq=off,ioeventfd=on,mq=on,vectors=18,id=net \
     -device scsi-hd,drive=drive-system,bus=scsi.0,rotation_rate=1,id=scsi-drive-system \
+    -device scsi-hd,drive=drive-test,bus=scsi.0,rotation_rate=1,id=scsi-drive-test \
+    -device ahci,id=ahci1 -device ahci,id=ahci2 \
+    -device ide-cd,drive=cd-win-install,bus=ahci1.0,id=ide-win-install \
+    -device ide-cd,drive=cd-win-virtio,bus=ahci2.0,id=ide-win-virtio \
     \
     -device virtio-mouse-pci,bus=pcie.0,id=mouse \
     -device virtio-keyboard-pci,bus=pcie.0,id=kbd \
@@ -74,9 +88,11 @@ jemalloc.sh qemu-system-x86_64 -no-user-config -nodefaults \
     -device virtio-serial-pci,bus=pcie.0,id=serial \
     -device ivshmem-plain,bus=pcie.0,memdev=ivshmem-mem,id=ivshmem \
     -device virtserialport,chardev=vdagent,name=com.redhat.spice.0,id=serial-spice \
+    -device virtserialport,chardev=qga,name=org.qemu.guest_agent.0,id=serial-qga \
     -device intel-hda,bus=pcie.0,msi=on,id=hda \
     -device hda-duplex,audiodev=audiodev,id=hdad \
     \
     -device qemu-xhci,bus=pcie.0,p2=15,p3=15,id=xhci \
     -device usb-host,bus=xhci.0,port=2,vendorid=0x046d,productid=0xc08c,id=usbmouse \
-    -device usb-host,bus=xhci.0,port=3,vendorid=0x046d,productid=0xc33f,id=usbkbd
+    -device usb-host,bus=xhci.0,port=3,vendorid=0x046d,productid=0xc33f,id=usbkbd \
+    -device usb-host,bus=xhci.0,port=4,vendorid=0x06cb,productid=0x0082,id=fingerprint
